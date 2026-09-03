d4e1ac176998aba1986e4e953115e567df580bc3
GeorgeCao1987 <cao_george@163.com>
2026-09-03 18:48:57 +0800

    Add cloud entry for V2.6 T-edge probability

---
+++ market_data/t_edge_probability_v26_cloud.py
@@ -0,0 +1,8 @@
+import backtest_v13 as v13
+from backtest_v14_cloud import load_a_cloud
+import t_edge_probability_v26 as v26
+
+
+if __name__ == "__main__":
+    v13.load_a = load_a_cloud
+    v26.main()
