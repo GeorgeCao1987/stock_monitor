9a36d330dc683df81c5690126abb7cc7518c3994
GeorgeCao1987 <cao_george@163.com>
2026-09-03 18:06:18 +0800

    Add cloud runner for V2.2 rolling extreme probability

---
+++ market_data/rolling_extreme_probability_v22_cloud.py
@@ -0,0 +1,8 @@
+import backtest_v13 as v13
+from backtest_v14_cloud import load_a_cloud
+import rolling_extreme_probability_v22 as v22
+
+
+if __name__ == "__main__":
+    v13.load_a = load_a_cloud
+    v22.main()
