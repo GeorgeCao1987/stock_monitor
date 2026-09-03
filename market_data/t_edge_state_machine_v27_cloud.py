20a7d84705a3fcc62437b1c2c85a1e42191b7f88
GeorgeCao1987 <cao_george@163.com>
2026-09-03 18:59:07 +0800

    Add cloud entry for V2.7 T-edge state machine

---
+++ market_data/t_edge_state_machine_v27_cloud.py
@@ -0,0 +1,8 @@
+import backtest_v13 as v13
+from backtest_v14_cloud import load_a_cloud
+import t_edge_state_machine_v27 as v27
+
+
+if __name__ == "__main__":
+    v13.load_a = load_a_cloud
+    v27.main()
