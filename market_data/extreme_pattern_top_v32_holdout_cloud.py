06456b551227a84e602ebfe77567b38cac9b7f00
GeorgeCao1987 <cao_george@163.com>
2026-09-03 19:45:00 +0800

    Add V3.2 holdout cloud loader

---
+++ market_data/extreme_pattern_top_v32_holdout_cloud.py
@@ -0,0 +1,8 @@
+import backtest_v13 as v13
+from backtest_v14_cloud import load_a_cloud
+import extreme_pattern_top_v32_holdout as v32
+
+
+if __name__ == "__main__":
+    v13.load_a = load_a_cloud
+    v32.main()
