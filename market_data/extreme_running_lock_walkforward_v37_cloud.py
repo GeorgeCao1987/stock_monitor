570a77906270ca901821f32a35e57c5c28e45fa5
GeorgeCao1987 <cao_george@163.com>
2026-09-03 20:46:36 +0800

    Add V3.7 cloud wrapper

---
+++ market_data/extreme_running_lock_walkforward_v37_cloud.py
@@ -0,0 +1,8 @@
+import backtest_v13 as v13
+from backtest_v14_cloud import load_a_cloud
+import extreme_running_lock_walkforward_v37 as v37
+
+
+if __name__ == "__main__":
+    v13.load_a = load_a_cloud
+    v37.main()
